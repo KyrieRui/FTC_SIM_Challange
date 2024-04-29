40ad9d18c2e66658e180a15f98d6acb52d7a9058
KyrieRui <kyrie19960324@gmail.com>
2024-04-29 14:01:23 +1200

    NEW version

--- FTC Sensors/MyFIRSTJavaOpMode.java
+++ FTC Sensors/MyFIRSTJavaOpMode.java
@@ -16,26 +16,6 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
     ColorSensor color1;
     DistanceSensor distance1;
     BNO055IMU imu;
-    
-public void runF(int time){
-    motorLeft.setPower(1);
-    motorRight.setPower(1);
-    sleep(time);
-}
-    
-public void left_90(){
-    motorLeft.setPower(-1);
-    motorRight.setPower(1);
-    sleep(440);
-    stop();
-}
-
-public void right_90(){
-    motorLeft.setPower(1);
-    motorRight.setPower(-1);
-    sleep(440);
-    stop();
-}
 
 @Override
     public void runOpMode() {
@@ -49,28 +29,9 @@ public void right_90(){
       // Put initialization blocks here
       waitForStart();
       // Put run blocks here
-      motorLeft.setDirection(DcMotor.Direction.REVERSE);
-      motorLeft.setPower(0.5);
-      motorRight.setPower(0.5);
       while (opModeIsActive()) {
         // Put loop blocks here
-        telemetry.addData("Red Value", (color1.red()));
-        telemetry.update();
-        if (color1.red() >= 255) {
-          left_90();
-          runF(300);
-          break;
-        }
-        
-        else if (color1.red() == 25) {
-          right_90(); 
-          runF(300);
-          break;
-        }
       }
-      sleep(130);
-      motorLeft.setPower(-1);
-      motorRight.setPower(1);
     }
     
 }
