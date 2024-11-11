378cb07ad2dea7c60c99f262937e224d3b9bb332
Ray <37173293+KyrieRui@users.noreply.github.com>
2024-11-11 15:51:53 +1300

    Create lesson3.java

---
+++ FTC Sensors/FTC Graber/lesson3.java
@@ -0,0 +1,55 @@
+
+/***********************************************************************
+*                                                                      *
+* OnbotJava Editor is still : beta! Please inform us of any bugs       |
+* on our discord channel! https://discord.gg/e7nVjMM                   *
+* Only BLOCKS code is submitted when in Arena                          *
+*                                                                      *
+***********************************************************************/
+
+
+public class MyFIRSTJavaOpMode extends LinearOpMode {
+    DcMotor driveLeft;
+    DcMotor driveRight;
+    DcMotor sideways;
+    Servo grabber;
+    ColorSensor color1;
+    DistanceSensor distance1;
+    BNO055IMU imu;
+
+    public void stop() {
+        driveLeft.setPower(0);
+        driveRight.setPower(0);
+    }
+
+    public void leftT(int time){
+        driveLeft.setPower(-1);
+        driveRight.setPower(1);
+        sleep(time);
+        stop();
+    }
+
+    public void goF(int time) {
+        driveLeft.setPower(0.5);
+        driveRight.setPower(0.5);
+        
+    }
+
+@Override
+    public void runOpMode() {
+      driveLeft = hardwareMap.get(DcMotor.class, "driveLeft");
+      driveRight = hardwareMap.get(DcMotor.class, "driveRight");
+      sideways = hardwareMap.get(DcMotor.class, "sideways");
+      grabber = hardwareMap.get(Servo.class, "grabber");
+      color1 = hardwareMap.get(ColorSensor.class, "color1");
+      distance1 = hardwareMap.get(DistanceSensor.class, "distance1");
+      imu = hardwareMap.get(BNO055IMU.class, "imu");
+      // Put initialization blocks here
+      waitForStart();
+      // Put run blocks here
+      grabber.setPosition(0.25);
+      driveLeft.setDirection(DcMotor.Direction.REVERSE);
+      
+    }
+    
+}
